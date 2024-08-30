d43ad96a2dbbc95e61e907b58e67971705e83e41
Navrose <navrosesinghjohal@gmail.com>
2024-08-30 13:29:25 -0400

    made use of react-router-dom

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -4,13 +4,24 @@ import ArticleList from './pages/ArticleList';
 import ArticlePage from './pages/ArticlePage';
 
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Blog</h1>
-      <div id="page-body">Welcome to my blog</div>
-    </div>
+    <BrowserRouter>
+
+      <div className="App">
+        <h1>My Blog</h1>
+        <div id="page-body">Welcome to my blog
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/article' element={<ArticleList />} />
+            <Route path='/articles/:articleId' element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
