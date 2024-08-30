626efa9a5e28a6478542e87df59d5a36ff4c963d
Navrose <navrosesinghjohal@gmail.com>
2024-08-30 13:36:00 -0400

    Added NavBar.js that uses Link from react-router-dom

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -2,6 +2,7 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticleList from './pages/ArticleList';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar'
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -11,12 +12,12 @@ function App() {
     <BrowserRouter>
 
       <div className="App">
-        <h1>My Blog</h1>
+        <NavBar />
         <div id="page-body">Welcome to my blog
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/about' element={<AboutPage />} />
-            <Route path='/article' element={<ArticleList />} />
+            <Route path='/articles' element={<ArticleList />} />
             <Route path='/articles/:articleId' element={<ArticlePage />} />
           </Routes>
         </div>
