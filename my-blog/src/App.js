02401698ecdf52e8c13a6f15d4aa08ef6ad7a976
Navrose <navrosesinghjohal@gmail.com>
2024-08-31 12:39:26 -0400

    Created error page

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -2,23 +2,24 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticleList from './pages/ArticleList';
 import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
 import NavBar from './NavBar'
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 function App() {
   return (
     <BrowserRouter>
 
       <div className="App">
         <NavBar />
-        <div id="page-body">Welcome to my blog
+        <div id="page-body">
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/about' element={<AboutPage />} />
             <Route path='/articles' element={<ArticleList />} />
             <Route path='/articles/:articleId' element={<ArticlePage />} />
+            <Route path='*' element={<NotFoundPage />} />
             // :articleId is url parameter - we use url parameter when we want a single component to be able to display multiple different items/pages as if we had different page for each item
           </Routes>
         </div>
---
+++ my-blog/src/pages/NotFoundPage.js
@@ -0,0 +1,5 @@
+const NotFoundPage = () =>
+(
+    <h1>Not found Error 404</h1>
+);
+export default NotFoundPage;
