328fcd6b5f32023bab72c3d76235255ee39c017d
Navrose <navrosesinghjohal@gmail.com>
2024-08-31 14:30:09 -0400

    Added general stuff to backend part

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 "use strict";
 const app = express();
+app.use(express.json());
 
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    // const {name } = req.params //object destructuring
+    res.send(`Hello! ${name}`);
+})
+app.post('/hello', (req, res) => {
+    console.log(req.body)
+    res.send(`Hello! ${req.body.name}`);
 })
 app.listen(8000, () => {
     console.log('working');
