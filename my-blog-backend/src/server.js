4470eaa28c2e172637d456b85d502fe391d75f1b
Navrose <navrosesinghjohal@gmail.com>
2024-08-31 14:04:30 -0400

    Made use of useLocation hook so display the article that was not found

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 "use strict";
 const app = express();
 
+
+app.get('/hello', (req, res) => {
+    res.send('Hello!');
+})
 app.listen(8000, () => {
     console.log('working');
 })
--- my-blog/src/pages/NotFoundPage.js
+++ my-blog/src/pages/NotFoundPage.js
@@ -1,5 +1,16 @@
-const NotFoundPage = () =>
-(
-    <h1>Not found Error 404</h1>
-);
+// const NotFoundPage = () =>
+// (
+//     <h1>Not found Error 404</h1>
+// );
+import { useLocation } from "react-router-dom";
+const NotFoundPage = () => {
+    const location = useLocation();
+    return (
+        <>
+            <h1>Not found Error 404</h1>
+            <p>No article named <code>{(location.pathname).substring(1)}</code> exists</p>
+
+        </>
+    )
+}
 export default NotFoundPage;
