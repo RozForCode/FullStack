9a790131329d55d7acb11f669abfa764a9c07bc3
Navrose <navrosesinghjohal@gmail.com>
2024-09-02 10:12:18 -0400

    Finished login page and link css

--- my-blog/src/pages/LoginPage.js
+++ my-blog/src/pages/LoginPage.js
@@ -1,6 +1,33 @@
+
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const Navigate = useNavigate();
+    const logIn = async () => {
+        try {
+            await signInWithEmailAndPassword(getAuth(), email, password);
+            Navigate('/articles');
+        }
+        catch (error) {
+            setError(error.message)
+        }
+    }
+
     return (
-        <h1>Login In</h1>
+        <>
+            <h1>Login In</h1>
+            {error && <p className='error'>{error}</p>}
+            <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+            <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+            <button onClick={logIn}>Log In</button>
+            <Link className="login-link" to="/create-account">Don't have an account? Create one here</Link>
+        </>
     )
 }
 export default LoginPage;
